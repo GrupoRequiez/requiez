8c24c3deeb8f87204452e3b5a41826ce31f37099
gflores <gustavofc65@gmail.com>
2021-06-22 11:18:36 -0500

    [UPD] overwrite _sql_constraints check_amount_currency_balance_sign

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -7,9 +7,42 @@ from odoo import fields, models
 _logger = logging.getLogger(__name__)
 
 
-class AccountInvoice(models.Model):
+class AccountMove(models.Model):
     _inherit = 'account.move'
     _name = 'account.move'
 
     date_payment = fields.Datetime('Payment Date',)
     prioritized = fields.Boolean('Prioritized', readonly=True)
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+    _name = 'account.move.line'
+
+    _sql_constraints = [
+        (
+            'check_amount_currency_balance_sign',
+            '''CHECK(
+                (
+                    (currency_id != company_currency_id)
+                    AND
+                    (
+                        (debit - credit <= 0 AND amount_currency <= 0 AND move_id.state != 'posted')
+                        OR
+                        (debit - credit >= 0 AND amount_currency >= 0 AND move_id.state != 'posted')
+                    )
+                )
+                OR
+                (
+                    currency_id = company_currency_id
+                    AND
+                    ROUND(debit - credit - amount_currency, 2) = 0
+                    AND
+                    move_id.state != 'posted'
+                )
+            )''',
+            "The amount expressed in the secondary currency must be positive when account is debited and negative when "
+            "account is credited. If the currency is the same as the one from the company, this amount must strictly "
+            "be equal to the balance."
+        ),
+    ]
