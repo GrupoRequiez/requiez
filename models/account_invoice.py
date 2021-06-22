9bb7416693213a8943c51c59040ca93bacc64369
gflores <gustavofc65@gmail.com>
2021-06-22 11:53:41 -0500

    edit sql_constraints

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -24,25 +24,13 @@ class AccountMoveLine(models.Model):
             'check_amount_currency_balance_sign',
             '''CHECK(
                 (
-                    (currency_id != company_currency_id)
-                    AND
-                    (
-                        (debit - credit <= 0 AND amount_currency <= 0 AND move_id.state != 'posted')
-                        OR
-                        (debit - credit >= 0 AND amount_currency >= 0 AND move_id.state != 'posted')
-                    )
+                    name = 'gsgkcvdbcvncm'
                 )
                 OR
                 (
-                    currency_id = company_currency_id
-                    AND
-                    ROUND(debit - credit - amount_currency, 2) = 0
-                    AND
-                    move_id.state != 'posted'
+                    name = 'lgndvsbdnvbks'
                 )
             )''',
-            "The amount expressed in the secondary currency must be positive when account is debited and negative when "
-            "account is credited. If the currency is the same as the one from the company, this amount must strictly "
-            "be equal to the balance."
+            "Test on _sql_constraints check_amount_currency_balance_sign"
         ),
     ]
