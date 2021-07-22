4baa4eb9d3fce3de5d3b8995433f6027b7e130b3
gflores <gustavofc65@gmail.com>
2021-07-22 13:44:32 -0500

    [FIX] correction to error when trying to confirm sales order

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -16,3 +16,14 @@ class MrpProduction(models.Model):
         ('D', 'D')],
         string="Order",
         default="A")
+
+    def _get_move_raw_values(self, product_id, product_uom_qty, product_uom, operation_id=False, bom_line=False):
+        res = super()._get_move_raw_values(product_id, product_uom_qty, product_uom, operation_id, bom_line)
+        res['location_dest_id'] = self.location_src_id.id
+        return res
+
+    def _get_move_finished_values(self, product_id, product_uom_qty, product_uom, operation_id=False, byproduct_id=False):
+        res = super()._get_move_finished_values(
+            product_id, product_uom_qty, product_uom, operation_id, byproduct_id)
+        res['location_id'] = self.location_src_id.id
+        return res
